38584af274ba1d62f25fc965fa07836fdff36dc7
Chathura Hettiarachchi <Chathura.h@fidenz.com>
2019-07-01 12:49:09 +0530

    state data management

---
+++ 06.State/src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer(props) {
+    return (
+        <footer>
+            <h3>This is my footer element. {props.footerdata}</h3>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ 06.State/src/MainContent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MainContent() {
+    return (
+        <main>
+            <p>This is where my content will go on</p>
+        </main>
+    )
+}
+
+export default MainContent
